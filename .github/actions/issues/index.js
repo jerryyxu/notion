db9efdeb8796f5857aa3de764310fbe3afc1ff9c
jerryyxu <1193486073@qq.com>
2021-09-01 20:23:10 +0800

    chore: test github actions

---
+++ .github/actions/issues/index.js
@@ -0,0 +1,42 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const run = async () => {
+    // Get octokit
+    const gitHubToken = core.getInput('repo-token', { required: true });
+    const octokit = github.getOctokit(gitHubToken);
+
+    // Get repo and issue info
+    const { repository, issue } = github.context.payload;
+    if (!issue) {
+        throw new Error(`Couldn't find issue info in current context`);
+    }
+    const [owner, repo] = repository.full_name.split('/');
+
+    // Get issue assignees
+    const assigneesString = core.getInput('assignees', { required: true });
+    const assignees = assigneesString
+        .split(',')
+        .map((assigneeName) => assigneeName.trim());
+
+    // Assign issue
+    console.log(
+        `Assigning issue ${issue.number} to users ${JSON.stringify(assignees)}`
+    );
+    try {
+        await octokit.rest.issues.addAssignees({
+            owner,
+            repo,
+            issue_number: issue.number,
+            assignees
+        });
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+};
+
+try {
+    run();
+} catch (error) {
+    core.setFailed(error.message);
+}
